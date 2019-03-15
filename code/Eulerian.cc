325143a87b4398a7a5416b6662fce5dfc59b2129
anhducle <anhducle@coccoc.com>
2019-03-15 14:12:43 +0700

    add eulerian path/circuit

---
+++ code/Eulerian.cc
@@ -0,0 +1,41 @@
+struct EulerianGraph {
+    vector< vector< pair<int, int> > > a;
+    int num_edges;
+
+    EulerianGraph(int n) {
+        a.resize(n + 1);
+        num_edges = 0;
+    }
+
+    void add_edge(int u, int v, bool undirected = true) {
+        a[u].push_back(make_pair(v, num_edges));
+        if (undirected) a[v].push_back(make_pair(u, num_edges));
+        num_edges++;
+    }
+
+    vector<int> get_eulerian_path() {
+        vector<int> path, s;
+        vector<bool> was(num_edges);
+        s.push_back(1); // start of eulerian path
+        while (!s.empty()) {
+            int u = s.back();
+            bool found = false;
+            while (!a[u].empty()) {
+                int v = a[u].back().first;
+                int e = a[u].back().second;
+                a[u].pop_back();
+                if (was[e]) continue;
+                was[e] = true;
+                s.push_back(v);
+                found = true;
+                break; 
+            }
+            if (!found) {
+                path.push_back(u);
+                s.pop_back();
+            }
+        }
+        reverse(path.begin(), path.end());
+        return path;
+    }
+};
